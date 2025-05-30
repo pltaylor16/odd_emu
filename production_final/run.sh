fb48b8c90365365514d12947a00e15c3a7d69f2d
pltaylor16 <peterllewelyntaylor@gmail.com>
2025-05-30 13:30:10 -0400

    match numbers actually used in production run

--- production_final/run.sh
+++ production_final/run.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 python driver.py
 python merge_batches.py
+compress_params.py
 python get_crit.py
+source /srv/scratch1/taylor.4264/anaconda3/etc/profile.d/conda.sh
 conda activate odd_emu
 python model_driver.py
