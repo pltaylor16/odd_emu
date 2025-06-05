doing final run with correct h units

new_approach_2/make_data.py
@@ -28,9 +28,10 @@ n_local = int(sys.argv[4])  # ✅ Correctly assign number of samples to generate
 # --- Emulator and k setup ---
 emulator = CosmoPowerJAX(probe="mpk_lin")
 k_emulator = emulator.modes.astype(np.float32)
-k_idx = np.where((k_emulator > 1e-2) & (k_emulator < 4.9))[0][::6]
+k_idx = np.where((k_emulator > 5e-2) & (k_emulator < 2.))[0][::6]
 k = k_emulator[k_idx]
 k_shape = len(k)
+
 
 # --- Output arrays ---
 logpk = np.zeros((n_local, nz, k_shape), dtype=np.float32)
@@ -49,6 +50,8 @@ for i in range(n_local):
     Omm = rng.uniform(0.24, 0.40)
     h = rng.uniform(0.61, 0.73)
 
+    k_h_units = k / h
+
     cosmo_dict = {'As': As, 'ns': ns, 'Omb': Omb, 'Omm': Omm, 'h': h, 'mnu': 0.0, 'w': -1.0, 'wa': 0.0}
     omega_b = Omb * h**2
     omega_cdm = (Omm - Omb) * h**2
@@ -60,7 +63,7 @@ for i in range(n_local):
         emulator.predict(np.append(cosmo_params, z))[k_idx] for z in z_array
     ])
     
-    _, boost_dict = ee2.get_boost(cosmo_dict, z_array, k)
+    _, boost_dict = ee2.get_boost(cosmo_dict, z_array, k_h_units)
     boost = np.stack([boost_dict[i] for i in range(len(z_array))])
     pk_nl = pk_lin_z * boost
     logpk[i] = np.log(pk_nl + 1e-30)
@@ -72,8 +75,8 @@ for i in range(n_local):
 
         pkm = emulator.predict(np.append(cosmo_params, z_minus))[k_idx]
         pkp = emulator.predict(np.append(cosmo_params, z_plus))[k_idx]
-        _, bm = ee2.get_boost(cosmo_dict, [z_minus], k)
-        _, bp = ee2.get_boost(cosmo_dict, [z_plus], k)
+        _, bm = ee2.get_boost(cosmo_dict, [z_minus], k_h_units)
+        _, bp = ee2.get_boost(cosmo_dict, [z_plus], k_h_units)
 
         pk_nl_m = pkm * bm[0]
         pk_nl_p = pkp * bp[0]